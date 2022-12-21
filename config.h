38635898e2f96fbbe148d2ea866f13c7530b7fcf
James Halim <tirtajames45@gmail.com>
2022-12-21 14:25:43 +0700

    newest

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* constants */
 #define VEDITOR "dav"
-#define BROWSER "chromium"
+#define BROWSER "brave"
 #define TORRENT "qbittorrent" 
 #define SCREENSHOT "screenshotnow"
 #define DISCORD "discord"
@@ -17,8 +17,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;       /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"monospace:size=10","NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {"monospace:size=11","NotoColorEmoji:pixelsize=11:antialias=true:autohint=true"};
+static const char dmenufont[]       = "monospace:size=11";
 /* static const char col_gray1[]       = "#222222"; */
 /* static const char col_gray2[]       = "#444444"; */
 /* static const char col_gray3[]       = "#bbbbbb"; */
@@ -73,24 +73,25 @@ static const Rule rules[] = {
          *WM_CLASS(STRING) = instance, class
            *WM_NAME(STRING) = title
              */
-    /* classinstance title  tags maskisfloatingisterminalnoswallow  monitor */
-    { "st-256color",              NULL,   NULL,   0,        0,1,0,-1 },
-    { NULL,                       "lf",   NULL,   0,        0,1,0,-1 },
-    { NULL,                       NULL,   "lf",   0,        0,1,0,-1 },
-    { "lf",                       NULL,   NULL,   0,        0,1,0,-1 },
-    { "qBittorrent",              NULL,   NULL,   1 << 8,   0,0,0,-1 },
-    { "resolve",                  NULL,   NULL,   1 << 1,   0,0,0,-1 },
-    { "discord",                  NULL,   NULL,   1 << 1,   0,0,0,-1 },
-    { "Audacity",                 NULL,   NULL,   1 << 1,   0,0,0,-1 },
-    { "audacity",                 NULL,   NULL,   1 << 1,   0,0,0,-1 },
-    { "kdenlive",                 NULL,   NULL,   1 << 1,   0,0,0,-1 },
-    { "Gwe",                      NULL,   NULL,   1 << 7,   0,0,0,-1 },
-    { "Nvidia-settings",          NULL,   NULL,   1 << 6,   0,0,0,-1 },
-    { "nvidia-settings",          NULL,   NULL,   1 << 6,   0,0,0,-1 },
-    { "Microsoft Teams - Preview",NULL,   NULL,   1 << 7,   0,0,0,-1 },
-    { "jamesdsp",                 NULL,   NULL,   1 << 7,   0,0,0,-1 },
-    { "teams-for-linux",          NULL,   NULL,   1 << 7,   0,0,0,-1 },
-    { "obs",                      NULL,   NULL,   1 << 8,   0,0,0,-1 },
+    /* classinstance              title  tags     mask     isfloating isterminal noswallow  monitor */
+	{ "st-256color",              NULL,   NULL,   0,       0,         1,         0,         -1 },
+	{ NULL,                       "lf",   NULL,   0,       0,         1,         0,         -1 },
+	{ NULL,                       NULL,   "lf",   0,       0,         1,         0,         -1 },
+	{ "lf",                       NULL,   NULL,   0,       0,         1,         0,         -1 },
+	{ "qBittorrent",              NULL,   NULL,   1 << 8,  0,         0,         0,         -1 },
+	{ "resolve",                  NULL,   NULL,   1 << 1,  0,         0,         0,         -1 },
+	{ "discord",                  NULL,   NULL,   1 << 1,  0,         0,         0,         -1 },
+	{ "Audacity",                 NULL,   NULL,   1 << 1,  0,         0,         0,         -1 },
+	{ "audacity",                 NULL,   NULL,   1 << 1,  0,         0,         0,         -1 },
+	{ "kdenlive",                 NULL,   NULL,   1 << 1,           0,         0,         0,-1 },
+	{ "Gimp",                     NULL,   NULL,   1 << 1,           0,         0,         0,-1 },
+	{ "Gwe",                      NULL,   NULL,   1 << 7,  0,         0,         0,         -1 },
+	{ "Nvidia-settings",          NULL,   NULL,   1 << 6,  0,         0,         0,         -1 },
+	{ "nvidia-settings",          NULL,   NULL,   1 << 6,  0,         0,         0,         -1 },
+	{ "Microsoft Teams - Preview",NULL,   NULL,   1 << 7,  0,         0,         0,         -1 },
+	{ "jamesdsp",                 NULL,   NULL,   1 << 7,  0,         0,         0,         -1 },
+	{ "teams-for-linux",          NULL,   NULL,   1 << 7,  0,         0,         0,         -1 },
+	{ "obs",                      NULL,   NULL,   1 << 8,  0,         0,         0,         -1 },
 };
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -128,60 +129,63 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  /* { MODKEY,                       XK_p,      spawn,          {.v = (const char*[]){ "dmenu_run", NULL } } }, */
-  { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  /* { MODKEY,                       XK_f,      togglebar,      {0} }, */
-  { MODKEY,                       XK_b,      togglebar,      {0} },
-  /* { MODKEY,                    XK_equal,  togglebar,      {0} }, */
-  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  { MODKEY,                       XK_f,      togglefullscr,  {0}},
-  /* { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } }, */
-  /* { MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } }, */
-  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,                       XK_space,  zoom,           {0} },
-  { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY,                       XK_q,      killclient,     {0} },
-  { MODKEY,                       XK_space,  setlayout,      {.v = &layouts[0]} },
-  /* { MODK                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
-  /* { MODKEY,                    XK_equal,  setlayout,      {.v = &layouts[2]} }, */
-  /* { MODKEY,                    XK_space,  setlayout,      {0} }, */
-  /* { MODKEY|ShiftM,             XK_space,  togglefloating, {0} }, */
-  /* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
-  /* { ALT,                           XK_0,      tag,            {.ui = ~0 } }, */
-  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { ALT,                          XK_comma,  tagmon,         {.i = -1 } },
-  { ALT,                          XK_period, tagmon,         {.i = +1 } },
-  /* { ALT,                       XK_equal,  quit,            {0} }, */ 
-  { MODKEY,                       XK_w,      spawn,          {.v = (const char*[]){ BROWSER, NULL } } },
-  { MODKEY,                       XK_m,    spawn,           {.v = (const char*[]){ "dmenusite" , NULL } } },
-  { ALT,                          XK_l,    spawn,           {.v = (const char*[]){ "dmenucopy" , NULL } } },
-  /* { MODKEY,              XK_d,    spawn,       {.v = (const char*[]){ VEDITOR, NULL } } }, */
-  { MODKEY,                       XK_t,      spawn,          {.v = (const char*[]){ TORRENT, NULL } } },
-  /* { ALT,                           XK_s,       spawn,       {.v = (const char*[]){ SCREENSHOT, NULL } } }, */
-  { MODKEY,                       XK_d,      spawn,          {.v = (const char*[]){ DISCORD, NULL } } },
-  /* { ALT,                          XK_p,      spawn,          {.v = (const char*[]){ "st -e paru -Syu --skipreview --needed", NULL } } }, */
-  { ALT,                          XK_p,      spawn,          SHCMD("st -e paru -Syu --skipreview --needed")},
-  { MODKEY,                       XK_minus,  spawn,          SHCMD("pamixer -d 1")},
-  { ALT,                          XK_minus,  spawn,          SHCMD("pamixer -d 3")},
-  { MODKEY,                       XK_equal,  spawn,          SHCMD("pamixer -i 1")},
-  { ALT,                          XK_equal,  spawn,          SHCMD("pamixer -i 3")},
-  { MODKEY,                       XK_s,      spawn,          SHCMD("~/.local/bin/speaker-or-headphone")},
-  { ALT,                          XK_m,      spawn,          SHCMD("pamixer -t")},
-  { ALT,                          XK_j,      spawn,          SHCMD("/home/james/.local/bin/alias/schedule")},
-  { ALT,                          XK_0,      spawn,          SHCMD("[ -z $( pgrep 'obs' ) ] && ~/.local/bin/setmicvol ; ~/.local/bin/alias/obs-kill-picom || picom -b")},
-  { MODKEY,                       XK_a,      spawn,          SHCMD("[ -z $( pgrep 'audacity' ) ] && ~/.local/bin/setmicvol ; ~/.local/bin/alias/auda")},
-  { MODKEY,                       XK_r,      spawn,          SHCMD("remaps")},
-  { MODKEY,                       XK_v,      spawn,          SHCMD("openvpn")},
-  { MODKEY,                       XK_c,      spawn,          SHCMD("~/.local/bin/colpick")},
-  { MODKEY,                       XK_s,      spawn,          SHCMD(SCREENSHOT)},
-  { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = -5 } },
-  { MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = +5 } },
-  { MODKEY|ShiftMask,             XK_0,      setgaps,        {.i = GAP_RESET } },
-  { MODKEY|ShiftMask,             XK_9,      setgaps,        {.i = GAP_TOGGLE} },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/* { MODKEY,                       XK_p,      spawn,          {.v = (const char*[]){ "dmenu_run", NULL } } }, */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	/* { MODKEY,                       XK_f,      togglebar,      {0} }, */
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	/* { MODKEY,                    XK_equal,  togglebar,      {0} }, */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0}},
+	/* { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } }, */
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	/* { ALT,                          XK_q,      spawn,          SHCMD("killall -HUP $( xdotool getwindowfocus getwindowname )")}, */
+	{ MODKEY,                       XK_space,  setlayout,      {.v = &layouts[0]} },
+	/* { MODK                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
+	/* { MODKEY,                    XK_equal,  setlayout,      {.v = &layouts[2]} }, */
+	/* { MODKEY,                    XK_space,  setlayout,      {0} }, */
+	/* { MODKEY|ShiftM,             XK_space,  togglefloating, {0} }, */
+	/* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
+	/* { ALT,                           XK_0,      tag,            {.ui = ~0 } }, */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ ALT,                          XK_comma,  tagmon,         {.i = -1 } },
+	{ ALT,                          XK_period, tagmon,         {.i = +1 } },
+	/* { ALT,                       XK_equal,  quit,            {0} }, */ 
+	{ MODKEY,                       XK_w,      spawn,          {.v = (const char*[]){ BROWSER, NULL } } },
+	{ MODKEY,                       XK_m,    spawn,           {.v = (const char*[]){ "dmenusite" , NULL } } },
+	{ ALT,                          XK_l,    spawn,           {.v = (const char*[]){ "dmenucopy" , NULL } } },
+	/* { MODKEY,              XK_d,    spawn,       {.v = (const char*[]){ VEDITOR, NULL } } }, */
+	{ MODKEY,                       XK_t,      spawn,          {.v = (const char*[]){ TORRENT, NULL } } },
+	/* { ALT,                           XK_s,       spawn,       {.v = (const char*[]){ SCREENSHOT, NULL } } }, */
+	/* { MODKEY,                       XK_d,      spawn,          {.v = (const char*[]){ DISCORD, NULL } } }, */
+	/* { ALT,                          XK_p,      spawn,          {.v = (const char*[]){ "st -e paru -Syu --skipreview --needed", NULL } } }, */
+	{ ALT,                          XK_k,      spawn,          SHCMD("[ -n $( pgrep picom ) ] && killall picom || picom -b")},
+	{ ALT,                          XK_u,      spawn,          SHCMD("st -e paru -Syu --skipreview --needed")},
+	{ MODKEY,                       XK_minus,  spawn,          SHCMD("pamixer -d 1; kill -35 $(pidof dwmblocks)")},
+	{ ALT,                          XK_minus,  spawn,          SHCMD("pamixer -d 2; kill -35 $(pidof dwmblocks)")},
+	{ MODKEY,                       XK_equal,  spawn,          SHCMD("pamixer -i 1; kill -35 $(pidof dwmblocks)")},
+	{ ALT,                          XK_equal,  spawn,          SHCMD("pamixer -i 2; kill -35 $(pidof dwmblocks)")},
+	{ MODKEY,                       XK_s,      spawn,          SHCMD("~/.local/bin/speaker-or-headphone")},
+	{ ALT,                          XK_m,      spawn,          SHCMD("pamixer -t")},
+	{ ALT,                          XK_j,      spawn,          SHCMD("/home/james/.local/bin/alias/schedule")},
+	{ ALT,                          XK_0,      spawn,          SHCMD("[ -z $( pgrep obs ) ] && ~/.local/bin/setmicvol && ~/.local/bin/alias/obs-kill-picom || picom -b")},
+	{ ALT,                          XK_grave,  spawn,          SHCMD("kill - 36 $(pidof dwmblocks)")},
+	{ MODKEY,                       XK_a,      spawn,          SHCMD("[ -z $( pgrep 'audacity' ) ] && ~/.local/bin/setmicvol ; ~/.local/bin/alias/auda")},
+	{ MODKEY,                       XK_r,      spawn,          SHCMD("remaps")},
+	{ MODKEY,                       XK_v,      spawn,          SHCMD("st -e openvpn")},
+	{ MODKEY,                       XK_c,      spawn,          SHCMD("~/.local/bin/colpick")},
+	{ MODKEY,                       XK_s,      spawn,          SHCMD(SCREENSHOT)},
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = -5 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_0,      setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_9,      setgaps,        {.i = GAP_TOGGLE} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -191,8 +195,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	/* { MODKEY|ShiftMask,             XK_q,      quit,           {0} }, */
+	/* { MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, */ 
 };
 
 /* button definitions */
